e3ec0162ad2c6d2e85901f867af02990edcbc517
c0dertitan <98435530+c0dertitan@users.noreply.github.com>
2022-05-01 15:52:05 -0500

    Create calc.js

---
+++ calc.js
@@ -0,0 +1,6 @@
+// Go into a new page in your web browser and right-click and then click inspect and then click console. And then copy this script and paste it into the console and then press ENTER.
+2 + 2
+3 + 3
+4 + 7
+102 * 98
+console.log('Done!')
